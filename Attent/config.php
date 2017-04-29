e74b7d27261e4ef1e37971186bc8087c335b1a51
Cuneyt Mertayak <cuneyt@ceng.metu.edu.tr>
2017-04-29 10:44:25 -0700

    Syntaxtic Change

--- Attent/config.php
+++ Attent/config.php
@@ -1,4 +1,4 @@
-<?php
+<?
 error_reporting(~E_NOTICE && ~E_DEPRECATED);
 
 require_once '../vendor/autoload.php';
@@ -14,4 +14,3 @@ $strAirtableBaseName = "Test_Attent_Internal";
 $strAirtableBaseEndpoint = 'https://api.airtable.com/v0/';
 $arrPersonalDoamin = array("gmail.com", "yahoo.com", "yahoo.co.in", "aol.com", "att.net", "comcast.net", "facebook.com", "gmail.com", "gmx.com", "googlemail.com", "google.com", "hotmail.com", "hotmail.co.uk", "mac.com", "me.com", "mail.com", "msn.com", "live.com", "sbcglobal.net", "verizon.net", "yahoo.com", "yahoo.co.uk", "rediif.com");
 $arrBannedDomains = array("resource.calendar.google.com");
-?>
