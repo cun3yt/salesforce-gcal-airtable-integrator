bff6b6b5e6d53fb30d45ee4f9da2f08572503084
Cuneyt Mertayak <cuneyt@ceng.metu.edu.tr>
2017-05-10 12:08:13 -0700

    Carrying Over Comments

--- Attent/processattendees.php
+++ Attent/processattendees.php
@@ -1,4 +1,9 @@
 <?
+/*
+* This file is responsible for processing meetings from DB and fetching attendees respective contact from
+* salesforce and than mapping it within DB for easy lookup and reference.
+*/
+
 error_reporting(~E_WARNING && ~E_NOTICE);
 
 require_once('config.php');
@@ -16,25 +21,45 @@ list($customer, $contacts) = Helpers::loadCustomerData($strClientDomainName);
 $sfdcIntegration = Helpers::getIntegrations($customer, CustomerContactIntegration::SFDC);
 
 if(is_array($sfdcIntegration) && (count($sfdcIntegration)>0)) {
+    // if we get salesforce OAuth access data we iterate and use the access data
+    // and assign it out global variables declared.
     $integrationDetails = json_decode($sfdcIntegration[0]->getData());
     $access_token = $integrationDetails->sfdc_access_token->access_token;
     $instance_url = $integrationDetails->sfdc_access_token->instance_url;
 }
 
+/*
+* Below you will see system fetching unprocessed attendees from customeer's meeting table
+* System will connect to meeting history table and fetch unprocessed attendees from there
+*/
 $arrGcalUser = Helpers::fnGetProcessAccounts();
 
 if( !(is_array($arrGcalUser) && (count($arrGcalUser)>0)) ) {
     exit;
 }
 
+/*
+* If there are unprocessed attendees, script will iterate through it and connect to salesforece and fetch the respective
+* contact that matches the attendee and than put pulled contact in attendee table and also add an
+* entry in attendee history table.
+*
+* Iteration will only be conducted if there are more than 0 unprocessed attendees fetched from customers airtable base.
+*/
 foreach($arrGcalUser as $arrUser) {
     $arrUpdatedIds = array();
     $strARecId = $arrUser['id'];
+
+
+    // Each meeting record will have attendees detail for that meeting, we will access every attendees email
+    // for that meeting. There can be multiple attendees for a meeting, we explode and prepare array of attendees
+    // so that we can iterate and process each single attendee from the meet and try to fetch its respective
+    // contact details from sfdc.
     $arrEmails = explode(",",$arrUser['fields']['Attendee Email(s)']);
 
     foreach($arrEmails as $strEm) {
         $domain = substr(strrchr($strEm, "@"), 1);
 
+        // comaparison we only process attendees those are external to client domains
         if(strtolower($domain) == strtolower($strClientDomainName)) {
             continue;
         }
@@ -44,11 +69,21 @@ foreach($arrGcalUser as $arrUser) {
         $arrAccountDetail = Helpers::fnGetContactDetail($strEm);
 
         if(is_array($arrAccountDetail) && (count($arrAccountDetail)>0)) {
+            // if contact found in airtable than we connect to sf and fetch the latest modified contact from sf
+            // check to see if the latest fetched record does have any updated values
+            // if they are updated we add a record in the attendee history table
+            // and than map it with meeting history table for look up
+
+            // connecting and getting latest modified contact detail from sf
             $arrAccountDetailSF = Helpers::fnGetContactDetailFromSf($instance_url, $access_token, $strEm);
 
             if( !(is_array($arrAccountDetailSF['records']) && (count($arrAccountDetailSF['records'])>0)) ) {
                 continue;
             }
+
+            // we now check if contact details fetched from sfdc, has some updated values or not
+            // if yes than we add it into attendee history table otherwise we proceed to next attendee
+            // system makes note of the attendee history record if it was created for mapping
 
             $IsToBeInserted = Helpers::fnCheckIfContactHistoryToBeInserted($arrAccountDetailSF['records']);
 
@@ -65,6 +100,10 @@ foreach($arrGcalUser as $arrUser) {
                 $arrUpdatedIds[] = $IsToBeInserted;
             }
         } else {
+            // if attendee not present in DB, we connect to sf and get the contact details from sf
+            // create a attendee record in the attendee table
+            // create a attendee history record in attendee history table and
+            // map attendee history record to meeting record
             $arrAccountDetailSF = Helpers::fnGetContactDetailFromSf($instance_url, $access_token,$strEm);
 
             if( !(is_array($arrAccountDetailSF['records']) && (count($arrAccountDetailSF['records'])>0)) ) {
@@ -85,6 +124,8 @@ foreach($arrGcalUser as $arrUser) {
         }
     }
 
+    // All the noted attendee record are here mapped with meeting history table
+    // if there are none we flag it as "NO SFDC Contact"
     if(is_array($arrUpdatedIds) && (count($arrUpdatedIds)>0)) {
         $boolUpdateAccount = Helpers::fnUpdateAccountRecord($strARecId,$arrUpdatedIds);
     } else {
