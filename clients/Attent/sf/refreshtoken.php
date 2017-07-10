b62bc8457049e39bf6ec9793dd7e6f4fde48a1f4
Cuneyt Mertayak <cuneyt@ceng.metu.edu.tr>
2017-07-09 22:00:37 -0700

    Random Fixes
    - Group clients under one folder
    - 15Five naming changed to lowercase
    - File renamings for consistency

--- clients/Attent/sf/refreshtoken.php
+++ clients/Attent/sf/refreshtoken.php
@@ -1,3 +1,3 @@
 <?
 include("${_SERVER['DOCUMENT_ROOT']}/Attent/config.php");   // @todo: Check a better way than including this file here?
-include("${_SERVER['DOCUMENT_ROOT']}/Core/sf/refreshtoken.php");
+include("${_SERVER['DOCUMENT_ROOT']}/Core/sf/gcal-refresh-token.php");
--- gcal/config.php
+++ gcal/config.php
@@ -7,4 +7,4 @@ SessionSingleton::start();
 require_once($_SERVER['DOCUMENT_ROOT']."/global-config.php");
 require_once($_SERVER['DOCUMENT_ROOT'].'/libraries/Helpers.php');
 $googleCalAPICredentialFile = $_SERVER['DOCUMENT_ROOT'] . '/config/google_api_credential_15five_domain.json';
-$arrClient['15Five'] = array('foldername'=>'15Five');
+$arrClient['15five'] = array('foldername'=>'15five');
